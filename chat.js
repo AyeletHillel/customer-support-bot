99eeaf358a208f7b42a1763820f2fadad6f22ee0
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-09-24 21:14:51 -0400

    added template

--- chat.js
+++ chat.js
@@ -4,9 +4,7 @@ import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { BufferMemory } from "langchain/memory";
 import * as fs from "fs";
-
 config();
 
 async function initializeChain() {
@@ -17,23 +15,18 @@ async function initializeChain() {
 
     const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
 
-    const fasterModel = new ChatOpenAI({
+    const model = new ChatOpenAI({
         modelName: "gpt-3.5-turbo",
     });
 
     const chain = ConversationalRetrievalQAChain.fromLLM(
-        fasterModel,
-        vectorStore.asRetriever(),
+        model,
+        vectorStore.asRetriever(),     
         {
             returnSourceDocuments: true,
-            // memory: new BufferMemory({
-            //     memoryKey: "chat_history",
-            //     inputKey: "question",
-            //     outputKey: "text",
-            //     returnMessages: true,
-            // }),
             questionGeneratorChainOptions: {
-                llm: fasterModel,
+                llm: model,
+                template: "You are a customer support assistant for an online store. Your primary role is to provide accurate information, resolve issues, and ensure a positive shopping experience for the users. Always prioritize clarity and helpfulness in your responses.",
             },
         }
     );
